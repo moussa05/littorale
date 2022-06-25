3090a512f024c868f3063838271e0e9f1e3385a2
Ndèye Aïssatou GASSAMA <ayshagassama1@gmail.com>
2022-06-25 11:33:53 +0000

    Ajout formulaire recherche et modification header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
 <body>
     <!-- ======= Header ======= -->
     <header id="header" class="header fixed-top mb-3">
-        <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+        <div class="container-fluid d-flex align-items-center justify-content-between">
 
         <a href="index.html" class="logo d-flex align-items-start text-decoration-none">
             <!--img src="img/logoUCAD.jpg" alt=""-->
@@ -29,8 +29,23 @@
                 <li><a class="nav-link active" href="#">Accueil</a></li>
                 <li><a class="nav-link" href="#">A propos</a></li>
                 <li><a class="nav-link" href="#">Contact</a></li>
-                <li><a class="nav-link" href="#">Se connecter</a></li>
+                <li><a class="nav-link" href="#">Se connecter &nbsp;</a></li>
             </ul>
+
+            <!-- Bouton modal recherche -->
+            <button
+                type="button"
+                class="btn btn-primary rounded"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+            >
+                <i
+                    class="bi bi-search"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal"
+                ></i>
+            </button>
+
             <i class="bi bi-list mobile-nav-toggle"></i>
         </nav><!-- .navbar -->
 
@@ -165,5 +180,46 @@
     <footer class="text-center lead bg-dark w-100 mb-0">
         Made with &#10084; by M2GDIL 
     </footer>
+
+    <!-- Modal Recherche -->
+    <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+    >
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Recherche article</h5>
+                    <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                    ></button>
+                </div>
+                <div class="modal-body">
+                    <form class="d-flex">
+                        <input
+                            class="form-control rounded"
+                            type="search"
+                            placeholder="Rechercher"
+                            aria-label="Search"
+                        />
+                        <!--button class="btn btn-outline-success" type="submit">Search</button!-->
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                        Fermer
+                    </button>
+                    <button type="button" class="btn btn-primary">Rechercher</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
